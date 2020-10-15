21a145b99ab361f9ff43a79f6b99bc880b965053
Timpuhin <timpuhin@gmail.com>
2020-10-15 12:32:04 +0600

    audio test

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import eel
 
 texts=[]
 f = open("texts.txt","r")
-pampam = f.read
+pampam = f.read()
+texts = pampam.split('/')
+del pampam
 f.close
-for i in pampam:
-    if pampam[i]==';':
-        print(i)
-        break
 
 @eel.expose
 def getText(textik):
-    return "lalala"
+    return texts
     
 eel.init("web")
 eel.start("main.html", size=(800,800))
+
--- test/test.py
+++
@@ -1,7 +0,0 @@
-texts=[]
-f = open("texts.txt","r")
-pampam = f.read()
-x = pampam.split('/')
-f.close
-for i in x:
-    if(x.)
