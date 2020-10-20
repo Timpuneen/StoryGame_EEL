b3bf7564b9d6c58fdf749cc3db200bd492a8d8df
Timpuhin <timpuhin@gmail.com>
2020-10-20 17:00:20 +0600

    осталось только украсить

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import eel
 
+health = 3
+images=[]
 texts=[]
 f = open("texts.txt","r")
 pampam = f.read()
-texts = pampam.split('/')
+texts = pampam.split('|')
 del pampam
 f.close
 
 @eel.expose
 def getText(textik):
-    return texts
+    return texts[1]
     
 eel.init("web")
 eel.start("main.html", size=(800,800))
-
